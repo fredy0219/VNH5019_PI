994295d7fcf5d1215f90c804d2c2d79bc4cd59b0
Fish <chenweiyu@Fishde-MacBook-Pro.local>
2018-06-09 19:31:21 +0800

    modify lerp value

--- VNH5019_test.py
+++ VNH5019_test.py
@@ -1,5 +1,10 @@
 import VNH5019_PI
 
+import time as time_ #make sure we don't override time
+
+def millis():
+    return int(round(time_.time() * 1000))
+
 if __name__ == '__main__':
 
 	vhn5019 = VNH5019_PI.VNH5019_PI(18,15,14)
@@ -7,9 +12,9 @@ if __name__ == '__main__':
 	vhn5019.set_direction(0)
 	vhn5019.set_duty(1)
 
-	time = millis();
+	start_time = millis();
 
-	while millis() - time < 5000:
+	while millis() - start_time < 5000:
 		vhn5019.drive_motor()
 
 	vhn5019.stop()
